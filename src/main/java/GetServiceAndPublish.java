e2c2918d8a618f9d2dec44998552659ae1a105fc
diannamcallister <dd.mcallister@hotmail.ca>
2019-12-29 16:14:58 -0500

    allow user input to choose service

--- src/main/java/GetServiceAndPublish.java
+++ src/main/java/GetServiceAndPublish.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
+import java.util.Scanner;
 
 public class GetServiceAndPublish {
 
@@ -8,13 +8,26 @@ public class GetServiceAndPublish {
 
         SolaceAPI api = new SolaceAPI();
         HashMap<String, String> all_services = api.get_all_services();
-        ConnectionData service_data = api.get_service_by_ID(all_services.get("Dianna"));
+
+        System.out.println("The list of services to choose from are:");
+        for (String key : all_services.keySet()) {
+            System.out.println( key );
+        }
+        System.out.println("\n");
+        System.out.println("Which service would you like to use today?");
+
+        Scanner in = new Scanner(System.in);
+        String chosen_service = in.next();
+
+
+        while (! all_services.containsKey(chosen_service)) {
+            System.out.println("That is not a valid service. Please try again and choose a valid service.");
+            chosen_service = in.next();
+        }
+
+        ConnectionData service_data = api.get_service_by_ID(all_services.get(chosen_service));
 
         String topicString = "try_me";
-
-//        Function<String, Void> printMessage = msg -> {System.out.println(msg);
-//        return null;
-//        };
 
         Subscriber subscriber = new Subscriber(service_data, topicString);
         Publisher publisher = new Publisher(service_data, topicString);
